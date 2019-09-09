ae1db9216e13e2504a34df7e5f3c227fbf9a97a0
HZH <853995849@qq.com>
2019-09-09 17:44:34 +0800

    SayHello

---
+++ src/com/apple/helloworld/SayHello.java
@@ -0,0 +1,19 @@
+public class SayHello {
+	public static void main(String args[]){
+		System.out.println("Below is the function of sayHello,");
+		SayHello sayhello = new sayHello()
+		sayhello.sayHello(0);
+	}
+
+	publie static viod sayHello(int sum){
+		System.out.printin("say Hello!"+sum);
+	}		
+	
+	publie static int sum(){
+		int sum = 0;
+		for(int i=0;1<=100;i++){
+			sum += 1;
+		}
+		return sum;
+	}
+}
